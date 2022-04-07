7b740f2f06f81c3b8bcbf760156b75a360a5f52c
Shuaib <wales149@gmail.com>
2022-04-07 12:14:28 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>OCR IMAGE</h1>
+    <div v-for="image in images" :key="image.id">
+      <h2>{{image.name}}</h2>
+      <img :src="image.url" alt="image of a black car" width="400" height="200">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      images: [
+        {
+          id: 989,
+          name: 'car with registeration number',
+          url: 'https://res.cloudinary.com/demo/image/upload/black_car.jpg',
+        },
+        {
+          id: 990,
+          name: 'car with blurred registeration number',
+          url: 'https://res.cloudinary.com/demo/image/upload/e_pixelate_region:20,g_ocr_text/black_car.jpg',
+      },
+      ]
+    }
+  }
+}
+</script>
